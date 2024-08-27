4bfccbfa3e377ecf4615025e0b822e35b64bcb47
Gricelda Padilla <114915430+Gricelda20@users.noreply.github.com>
2024-08-26 22:07:24 -0400

    Assignment2
    Was able to generate a random number and create higher and lower system out up for both next create a while loop for number of attempts and for number not between the sequence of 1-100

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -1,10 +1,37 @@
 package com.coderscampus;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+	
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random ();
+		int randomNumber = random.nextInt(100)+ 1;
+		
+		System.out.println("This is your random number " + randomNumber);
+		
+		System.out.println("Pick a number between 1 and 100 ");
+		System.out.println("You only get 5 chances. Good Luck!");
+		
+		int playerGuess = scanner.nextInt();
+		
+		int attempts = 0;
+		while (attempts < 5) {
+            System.out.println(attempts + 1);
+            attempts++;
+		}
+		
+		if(playerGuess == randomNumber) {
+			System.out.println("You Win!");	
+		
+		} else if (randomNumber > playerGuess) {
+			System.out.println("Please pick a higher number.");
+		} else {
+			System.out.println("Please pick a lower number.");
+		}
+		
+		
+	}}
