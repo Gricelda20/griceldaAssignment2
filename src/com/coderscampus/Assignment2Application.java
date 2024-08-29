0510c5dcc9cead31e7629dad2f481f8c1a2bbf3e
Gricelda Padilla <114915430+Gricelda20@users.noreply.github.com>
2024-08-29 09:49:34 -0400

    assisgnment2

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -11,27 +11,45 @@ public class Assignment2Application {
 		Random random = new Random ();
 		int randomNumber = random.nextInt(100)+ 1;
 		
-		System.out.println("This is your random number " + randomNumber);
+//		System.out.println("This is your random number " + randomNumber);
 		
 		System.out.println("Pick a number between 1 and 100 ");
 		System.out.println("You only get 5 chances. Good Luck!");
 		
-		int playerGuess = scanner.nextInt();
 		
-		int attempts = 0;
-		while (attempts < 5) {
-            System.out.println(attempts + 1);
-            attempts++;
-		}
-		
-		if(playerGuess == randomNumber) {
-			System.out.println("You Win!");	
-		
-		} else if (randomNumber > playerGuess) {
+		int maxAttempts = 5;
+	
+		for (int i = 0; i < maxAttempts; i++) {
+			int playerGuess = 0;
+			boolean validInput = false;
+			
+			while (true) {
+				if (scanner.hasNextInt()) {
+					playerGuess = scanner.nextInt();
+				if (playerGuess >= 1 && playerGuess <= 100) {
+					validInput = true; // Valid guess ){
+					break;	
+				} else {
+					System.out.println("Your guess is not between 1 and 100, please try again.");
+					}
+//				} else {
+//					System.out.println("Invalid input. Please enter a valid number.");
+//					scanner.next();
+			}
+			}
+			if(playerGuess == randomNumber) {
+				System.out.println("You Win!");	 
+				return; // Exit the loop if the guess is correct
+			} else if (playerGuess < randomNumber) {
 			System.out.println("Please pick a higher number.");
-		} else {
-			System.out.println("Please pick a lower number.");
-		}
-		
-		
-	}}
+			} else {
+				System.out.println("Please pick a lower number.");
+			} 
+	}
+		System.out.println("You Lose!");
+		System.out.println("The number to guess was: " + randomNumber);
+		scanner.close();
+	}
+}
+	
+
